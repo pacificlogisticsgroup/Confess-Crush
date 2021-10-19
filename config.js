dfd038daf02ee3f59c094271517e2f9ff1e4e5ee
pacificlogisticsgroup <92769564+pacificlogisticsgroup@users.noreply.github.com>
2021-10-19 09:41:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "Pacific Logistics Group",
+    introTitle: 'Xin chào các nữ Pacificer',
+    introDesc: `Các bạn đã sẵn sàng tham gia Party 20/10 hôm nay chưa`,
+    btnIntro: 'Sẵn sàng',
+    title: 'Các bạn thấy các bạn nam Pacific 🥰',
+    desc: 'có đẹp trai không? ',
+    btnYes: 'Có',
+    btnNo: 'Không nha',
+    question: 'Các bạn có yêu trai Pacific không <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Chiều nay 16h30, Hẹn gặp các bạn tại phòng 613-614 nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/pacificlogisticsvn' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
